4c22f508f6f248ec9eefe967a7025c7bdf38f759
Felix <felix@niederer.dev>
2025-08-19 20:33:20 +0200

    test flow

--- src/ring_osc.v
+++ src/ring_osc.v
@@ -6,13 +6,11 @@ module ro_trng (
 );
     wire n1, n2, n3, n4, n5;
 
-    // 5-stage inverter loop
-    // (* keep, dont_touch *) prevents synthesis optimization
-    (* keep, dont_touch *) sg13g2_inv_2 u1 (.Y(n1), .A(n5));
-    (* keep, dont_touch *) sg13g2_inv_2 u2 (.Y(n2), .A(n1));
-    (* keep, dont_touch *) sg13g2_inv_2 u3 (.Y(n3), .A(n2));
-    (* keep, dont_touch *) sg13g2_inv_2 u4 (.Y(n4), .A(n3));
-    (* keep, dont_touch *) sg13g2_inv_2 u5 (.Y(n5), .A(n4));
+     sg13g2_inv_2 u1 (.Y(n1), .A(n5));
+     sg13g2_inv_2 u2 (.Y(n2), .A(n1));
+     sg13g2_inv_2 u3 (.Y(n3), .A(n2));
+     sg13g2_inv_2 u4 (.Y(n4), .A(n3));
+     sg13g2_inv_2 u5 (.Y(n5), .A(n4));
 
     assign rnd = n5;
 
